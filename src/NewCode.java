42b255d9cb09bd60fcc50c93b9f3b3293dce2331
Unknown <arieauna.chatmon.2019@student.lhschools.org>
2018-10-09 14:20:07 -0400

    Random pull from computer completed. Now if statements.

--- src/NewCode.java
+++ src/NewCode.java
@@ -1,60 +1,47 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NewCode {
     public static void main(String[] args) {
 
         int generatedNumber;
-        int numberOne, numberTwo, numberThree;
         boolean hope;
-        generatedNumber = (int) (Math.random() * 3);
-        numberOne = (int) (Math.random() * 3);
-        numberTwo = (int) (Math.random() * 3);
-        numberThree = (int) (Math.random() * 3);
-
 
         Scanner user;
         user = new Scanner(System.in);
         String userAnswer;
-        String cowOption;
-        String bombOption;
-        String lakeOption;
-        cowOption = "cow";
-        bombOption = "bomb";
-        lakeOption = "lake";
-
+        String[] option = {"cow", "bomb", "lake"};
         hope = true;
 
-        String[] option = {cowOption, bombOption, lakeOption};
-        option[numberOne] = cowOption;
-        option[numberTwo] = bombOption;
-        option[numberThree] = lakeOption;
-        System.out.println(generatedNumber);
-
         while (hope) {
+
+            generatedNumber = (int) (Math.random() * 3);
+            System.out.println(option[generatedNumber]);
+
             System.out.println("Hi! Test!");
-            userAnswer = user.nextLine();
-            for (int i = generatedNumber; i < option.length; i++) {
-                System.out.println(option[i]);
-                break;
+
+            for (int i = 0; i < option.length; i++) {
+                userAnswer = user.nextLine();
+                if(Arrays.asList(option).contains(userAnswer)){
+                    System.out.println("MOOOOOO!");
+                }
+                if ("cow".equals(option)) {
+                    System.out.println("I really hope this works! Cow drinks Lake!");
+                    break;
+                }
+                if ("bomb".equals(option)){
+                    System.out.println("Cow got blown up by the Bomb!");
+                    break;
+                }
+                if ("lake".equals(option)){
+                    System.out.println("Lake extinguishes Bomb;");
+                    break;
+                }
             }
-            if (userAnswer.equals("lake") && option.equals(cowOption)) {
-                System.out.println("I really hope this works! Cow drinks Lake!");
-            } else if (userAnswer.equals("cow") && option.equals(bombOption)) {
-                System.out.println("Cow got blown up by the Bomb!");
-            } else if (userAnswer.equals("bomb") && option.equals(lakeOption)) {
-                System.out.println("Lake extinguishes Bomb;");
-            }
+            hope = false;
         }
     }
 }
 
-
-
-
-            /*String [] options = {cowOption, bombOption, lakeOption};
-         int help = options.length;
-         for(int i = generatedNumber; i<help;i++) {
-             System.out.println("I hope this works " + options[i]);
-             */
 
 
